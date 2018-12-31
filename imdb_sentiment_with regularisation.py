8738c05d5655ed1d12931c7c8edf92891578f53c
Verma <Karan.Verma@unilever.com>
2018-12-31 14:02:07 +0530

    Without regularization test data evaluation : 16 units [0.8112427774882317, 0.84612] 4 units [0.4251603396511078, 0.86644] 512 units [0.9472465605561435, 0.87]

--- imdb_sentiment_with regularisation.py
+++ imdb_sentiment_with regularisation.py
@@ -5,8 +5,12 @@ Spyder Editor
 This is a temporary script file.
 """
 
+#initially run with no L2 regularizer, now with l2 = 0.001
+
+
 from keras import models
 from keras import layers
+from keras import regularizers
 from keras.datasets import imdb
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,6 +20,14 @@ import numpy as np
 #get word indices & #reverse the index
 word_index = imdb.get_word_index()
 reverse_word_index = dict([(value, word) for (word, value) in word_index.items()])
+
+#variables to hold values for all models
+lyrs = [16, 4, 512]
+model_all = []
+output_dir_graphs = r'C:\Users\karan.verma\.spyder-py3\deep-learning\graphs'
+#putting all losses in the following variable : validation and training as a list
+all_loss = []
+acc_all = []
 
 #one hot ecoding of the data, 10,000 units long index
 def onehot(arr):
@@ -37,105 +49,57 @@ val_x = train_x_new[15000:]
 partial_train_y = train_y[:15000]
 val_y = train_y[15000:]
 
-#create model big and small
-model_big = models.Sequential()
-model_big.add(layers.Dense(16, activation = 'relu', input_shape = (10000,)))
-model_big.add(layers.Dense(16, activation = 'relu'))
-model_big.add(layers.Dense(1, activation='sigmoid'))
-
-model_small = models.Sequential()
-model_small.add(layers.Dense(4, activation = 'relu', input_shape = (10000,)))
-model_small.add(layers.Dense(4, activation = 'relu'))
-model_small.add(layers.Dense(1, activation='sigmoid'))
-
-model_super = models.Sequential()
-model_super.add(layers.Dense(512, activation = 'relu', input_shape = (10000,)))
-model_super.add(layers.Dense(512, activation = 'relu'))
-model_super.add(layers.Dense(1, activation='sigmoid'))
-
-#compile models
-model_big.compile(optimizer='rmsprop',
+#create model big and small and then compile and run
+for i in range(3):
+    model = models.Sequential()
+    model.add(layers.Dense(lyrs[i], kernel_regularizer=regularizers.l2(0.000), activation = 'relu', input_shape = (10000,)))
+    model.add(layers.Dense(lyrs[i], activation = 'relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+    
+    model.compile(optimizer='rmsprop',
                   loss = 'binary_crossentropy',
                   metrics = ['accuracy'])
+    
+    
+    model_all.append(model)
+    
+    print('\nTraining Model with %d units.--------------------->' % lyrs[i])    
+    
+    
+    history = model.fit(partial_train_x,
+                            partial_train_y,
+                            epochs = 20,
+                            batch_size = 512,
+                            validation_data=(val_x, val_y),
+                            verbose=1)
 
-model_small.compile(optimizer='rmsprop',
-                  loss = 'binary_crossentropy',
-                  metrics = ['accuracy'])
-
-model_super.compile(optimizer='rmsprop',
-                  loss = 'binary_crossentropy',
-                  metrics = ['accuracy'])
-
-
-#run model big
-print('Running Older Model')
-history1 = model_big.fit(partial_train_x,
-                        partial_train_y,
-                        epochs = 20,
-                        batch_size = 512,
-                        validation_data=(val_x, val_y),
-                        verbose=1)
-
-big_acc = history1.history
-
-#run model small
-print('\nRunning Newer Model')
-history2 = model_small.fit(partial_train_x,
-                        partial_train_y,
-                        epochs = 20,
-                        batch_size = 512,
-                        validation_data=(val_x, val_y),
-                        verbose=1)
-
-small_acc = history2.history
-
-#run model super
-print('\nRunning Super Model')
-history3 = model_super.fit(partial_train_x,
-                        partial_train_y,
-                        epochs = 20,
-                        batch_size = 512,
-                        validation_data=(val_x, val_y),
-                        verbose=1)
-
-super_acc = history3.history
+    all_loss.append([history.history['val_loss'],history.history['loss']])
+    acc_all.append(model.evaluate(test_x_new, test_y))
+    
 
 
 e = range(1,21)
-big_acc_val_loss = big_acc['val_loss']
-small_acc_val_loss = small_acc['val_loss']
-super_acc_val_loss = super_acc['val_loss']
+loss_fig, ax = plt.subplots(2,1, figsize=(20,10))
 
-big_acc_tr_acc = big_acc['val_acc']
-small_acc_tr_acc = small_acc['val_acc']
-super_acc_tr_acc = super_acc['val_acc']
-
-btr_loss = big_acc['loss']
-str_loss = small_acc['loss']
-su_tr_loss = super_acc['loss']
-
-#plt.plot(e, small_acc_val_loss, 'b', label='Small Model Loss')
-plt.plot(e, big_acc_val_loss, '+', label='Large Model Loss')
-plt.plot(e, super_acc_val_loss, 'o', label='Super Model Loss')
+plt.subplot(211)
+plt.title('Validation Loss')
+plt.plot(e, all_loss[0][0], 'b', label='Original Model')
+plt.plot(e, all_loss[1][0], 'g', label='Small Model')
+plt.plot(e, all_loss[2][0], 'r', label='Large Model')
 plt.xlabel('Epochs')
 plt.ylabel('Validation Loss')
 plt.legend()
-plt.show()
 
-#plt.plot(e, small_acc_tr_acc, 'b', label='Small Model Tr Accu')
-plt.clf()
-plt.plot(e, big_acc_tr_acc, '+', label='Large Model Tr Accu')
-plt.plot(e, super_acc_tr_acc, 'o', label='Super Model Tr Accu')
-plt.xlabel('Epochs')
-plt.ylabel('Training Accuracy')
-plt.legend()
-plt.show()
-
-plt.clf()
-plt.plot(e, btr_loss, '+', label='Large Model')
-plt.plot(e, su_tr_loss, 'o', label='Super Model')
+plt.subplot(212)
+plt.title('Training Loss')
+plt.plot(e, all_loss[0][1], 'b', label='Original Model')
+plt.plot(e, all_loss[1][1], 'g', label='Small Model')
+plt.plot(e, all_loss[2][1], 'r', label='Large Model')
 plt.xlabel('Epochs')
 plt.ylabel('Training Loss')
 plt.legend()
-plt.show()
+
+plt.tight_layout()
+
+loss_fig.savefig('{}/Model Comparison without Regularization IMDB'.format(output_dir_graphs))
 
