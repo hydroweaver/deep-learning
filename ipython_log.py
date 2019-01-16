6c8d217425b648f337ce875aa8c4b1f0d1593ffa
Verma <Karan.Verma@unilever.com>
2019-01-16 15:06:24 +0530

    mnist digits with convnets give 98.8% accuracy

---
+++ ipython_log.py
@@ -0,0 +1,12 @@
+# IPython log file
+
+runfile('C:/Users/karan.verma/.spyder-py3/deep-learning/digits_mnist_trial_with_convnets.py', wdir='C:/Users/karan.verma/.spyder-py3/deep-learning')
+print('Random samples from the test data: ')
+fig, ax = plt.subplots(2, 3, figsize=(20, 10))
+for i in range(1, 7):
+        c = np.random.choice(len(test_x_reshaped))
+        plt.subplot(2,3,i)
+        plt.imshow(test_x[c])
+        plt.title('Original {} & Predicted {}'.format(label_list[np.argmax(test_y[c])], label_list[np.argmax(prediction[c])]))        
+        #plt.tight_layout()
+os.execvpe('%logstart -o')
